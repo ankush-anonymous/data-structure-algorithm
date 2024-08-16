8381bcc23c58be176914f6703e18910a45912651
ankush-anonymous <canankushdoit@gmai.com>
2024-08-16 21:36:50 +0530

    hashMapBasics

---
+++ Learn/HashMap/HashMapBasics.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class HashMapBasics {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        // .put(key,value) => it inserts key value pair to hashmap
+        hm.put("India", 150);
+        hm.put("China", 15);
+        hm.put("US", 10);
+
+        // to print hashMap
+        System.out.println(hm);
+
+        // .get(key) => returns the value of its corresponding key
+        System.out.println(hm.get("India"));
+
+        // .containsKey(key) => return true if key value pair exists;
+        System.out.println(hm.containsKey("India"));
+
+        // .remove(key) => removes key value pair and returns value
+        System.out.println(hm.remove("China"));
+
+        // .size() => returns size of HashMap
+        System.out.println(hm.size());
+
+        // .isEmpty() => returns true if hashMap is empty
+        System.out.println(hm.isEmpty());
+
+        // .clear() => to clear the hashMap
+        hm.clear();
+
+    }
+}
